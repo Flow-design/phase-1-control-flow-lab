c4c2e3091272d469fcff8b2830b0e74c43c1b58c
devin <devin.kpchumba@student.moringaschool.com>
2025-03-10 17:47:19 +0300

    control

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if(feet <  400){
+    return 'This one is on me!'
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+if(feet >400 && feet <2000  ){
+  return 'That will be twenty bucks.'
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+if(feet >= 2000 && feet  < 2500){
+  return 'I will gladly take your thirty bucks.'
+}
+if(feet > 2500){
+  return 'No can do.'
+}
+}
+function ternaryCheckCity(town){
+  if (town === 'NYC' ) {
+    return 'Ok, sounds good.'
+} else {
+  return 'No go.'
+} 
+}
+function switchOnCharmFromTip(Tip){
+  switch(Tip){
+    case 'generous':
+      return 'Thank you so much.'
+      case 'not as generous':
+        return 'Thank you.'
+        case 'thanks for everything':
+          return 'Bye.'
+  }
 }
